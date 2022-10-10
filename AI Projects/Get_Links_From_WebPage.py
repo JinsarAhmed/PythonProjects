24f4ab3e2cbeeb4783aa58939f08c1537a53d0bc
Ahmed Jinsar <84829321+JinsarAhmed@users.noreply.github.com>
2022-10-11 01:03:15 +0500

    new project added...
    new directory AI projects added....
    in addition a new project (Links Scraper) is also added in this commit, which is based on two modules: 1) requests  and 2) BeautifulSoup4

---
+++ AI Projects/Get_Links_From_WebPage.py
@@ -0,0 +1,20 @@
+# Links Scraper from any given website or webpage
+# Modules Required: 1) requests  2) BeautifulSoup4
+
+import requests as rq
+from bs4 import BeautifulSoup
+
+url = input("Enter Link: ")
+if ("https" or "http") in url:
+    data = rq.get(url)
+else:
+    data = rq.get("https://" + url)
+soup = BeautifulSoup(data.text, "html.parser")
+links = []
+for link in soup.find_all("a"):
+    links.append(link.get("href"))
+
+# Writing the output to a file (myLinks.txt) instead of to stdout
+# You can change 'a' to 'w' to overwrite the file each time
+with open("myLinks.txt", 'a') as saved:
+    print(links[:10], file=saved)
